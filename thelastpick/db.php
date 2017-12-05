53beade462c4ee2b650aeb85a7635245a71c1769
bhaskar bansal <bhaskarbansal96@gmail.com>
2017-12-05 21:07:12 +0530

    Update db.php

--- thelastpick/db.php
+++ thelastpick/db.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost";
-$username = "id3800351_krishna";
-$password = "Krishna12345";
+$servername = "us-cdbr-iron-east-05.cleardb.net";
+$username = "be9f3fe40ffe9f";
+$password = "c526a77a";
 $db = "id3800351_ecomm";
 
 // Create connection
